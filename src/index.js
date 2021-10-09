11264067eb255cdac3605924f106c120f2a6e49a
Ryan <slaric2014@gmail.com>
2021-10-08 21:47:16 -0400

    update header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
+import Header from "./components/header";
+
 const App = ()=>{
-  return <h1>hello world!</h1>
+  return <Header />
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
