797fe07e309e1fc8b73255d1b1a111370624895a
Ryan <slaric2014@gmail.com>
2021-12-11 22:34:05 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 
-import Header from "./components/header";
+import App from "./App";
 
-const App = ()=>{
-  return <Header />
-}
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
