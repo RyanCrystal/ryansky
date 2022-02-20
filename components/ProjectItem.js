4e8f6f09be6f26e6a7817a897e390c7c0f3f25ea
Ryan <slaric2014@gmail.com>
2022-02-19 23:52:01 -0500

    update

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -26,7 +26,7 @@ export function ProjectItem({ project }) {
       </div>
       <div className={styles.github}>
         <a href={project.github_link}>
-          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
       </div>
     </div>
