f8a39e03529d69f63a13188ae246e88dc7be0fe4
Ryan <slaric2014@gmail.com>
2022-05-22 10:52:36 -0400

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+  <link rel="icon" href="{{ url('img/favicon.jpg') }}">
   <link rel="stylesheet" href="css/app.css">
 
-  <title>Document</title>
+  <title>Porfolio-Ryan</title>
 </head>
 
 <body>
